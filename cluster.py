8d0e209989d1597ca0365e628921ca62325c98a5
Nicolas Sanchez <sancheznico93@gmail.com>
2018-08-09 12:20:41 -0300

    added deploy command

--- cluster.py
+++ cluster.py
@@ -81,6 +81,28 @@ def create(config, name, alias, description=""):
     print("Cluster created and config updated")
 
 
+@cluster.command()
+@click.option('--cluster-name', 'cluster_name', prompt='Cluster name', help='The name of the cluster')
+@click.option('--namespace', 'cluster_namespace', prompt='Cluster namespace', help='The namespace in the cluster')
+@click.option('--model-tag', 'model_tag', prompt="Model tag", help='Tag of the model')
+@click.option('--model-name', 'model_name', prompt="Model name", help='Name of the model')
+def deploy(cluster_name, cluster_namespace, model_tag, model_name):
+    """Deploys a model in a client's cluster"""
+
+    url = '{}/model/deploy'.format(API_URL_BASE)
+    headers = get_header_basic_auth()
+
+    body = {
+        'model_tag': model_tag,
+        'model_name': model_name,
+        'cluster_name': cluster_name,
+        'cluster_namespace': cluster_namespace
+    }
+
+    response = requests.post(url, headers=headers, json=body)
+    print(response.json())
+
+
 @click.option('--model_name', prompt='Model name', help='Model Name.')
 @click.option('--model_tag', prompt='Model tag', help='Model Tag.')
 def deploy_serverstart(model_name, model_tag):
