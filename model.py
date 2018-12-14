41322e63639c15becbf41e37277df0bc1358934f
umar-sik <umar.gondal6198@gmail.com>
2018-12-14 18:56:39 -0500

    prediction method added

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import requests
 import subprocess as _subprocess
 from urllib.request import urlopen
 import os.path
-from utils import API_URL_BASE
+from utils import API_URL_BASE, API_SERVER
 from utils import get_header_basic_auth
 
 
--- utils/constants.py
+++ utils/constants.py
@@ -5,9 +5,11 @@ HOME = str(Path.home())
 
 BASE_URL = 'http://localhost'
 BASE_URL_BACKEND = 'http://35.243.188.142'
+BASE_API_SERVER = 'http://35.231.59.144'
 PORT = 5000
 API_URL_BASE = "{}:{}".format(BASE_URL, PORT)
 API_BACKEND = "{}:{}".format(BASE_URL_BACKEND, PORT)
+API_SERVER = "{}:{}".format(BASE_API_SERVER, PORT)
 
 USERDATA_PATH = HOME + "/.dataspine/userdata"
 PUBLIC_KEY_PATH = HOME + "/.dataspine/public-key"
