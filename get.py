28bcaac4c18693b84718b93ea4969a6997267bd8
umar-sik <umar.gondal6198@gmail.com>
2018-12-17 17:01:54 -0500

    list functionality added

--- get.py
+++ get.py
@@ -30,25 +30,36 @@ def getdeployments(clustername):
     try:
         response = requests.get(url, headers=headers, json=body).json()
         deployments_list = []
-        for deployment in response["items"]:
-              print(deployment.name)
-              for annotation in deployment["metadata"]:
-                print(annotation)
-                deployments_list.append(
-                [annotation['name'], annotation['name']])
-                print(tabulate(deployments_list, headers=[
-                    "Name", "Alias"], tablefmt='orgtbl'))
-        # for f in response['items.metadata.']:
-        #   print(f)
+        items =  response["items"]
+        for deployment in items:
+            deployments_list.append(
+            [deployment["metadata"]["name"], deployment["metadata"]["namespace"]])
+        print(tabulate(deployments_list, headers=[
+        "Deployments", "Namespace"], tablefmt='orgtbl'))
 
     except KeyError:
         error = 'Something went wrong'
         print(error)
 
+@get.command('services')
+@click.option('--cluster name', 'clustername', prompt='Cluster name', help='The name of the cluster')
+def getdeployments(clustername):
+    url = API_URL_BASE + '/getservices'
+    headers = get_header_basic_auth()
 
-# deployments_list = []
-#    for deployment in reposonse["items"]:
-#         clusters_list.append(
-#             [deployment['metadata.name'], deployment['metadata.namespace']])
-#     print(tabulate(clusters_list, headers=[
-#           "Name", "Alias"], tablefmt='orgtbl'))
+    body = {
+        'cluster_name': clustername
+    }
+    try:
+        response = requests.get(url, headers=headers, json=body).json()
+        deployments_list = []
+        items =  response["items"]
+        for deployment in items:
+            deployments_list.append(
+            [deployment["metadata"]["name"], deployment["metadata"]["namespace"]])
+        print(tabulate(deployments_list, headers=[
+        "Services", "Namespace"], tablefmt='orgtbl'))
+
+    except KeyError:
+        error = 'Something went wrong'
+        print(error)
