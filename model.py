957c8d2d96f3c68955fb7fe0879eaf9e09011eb1
umar-sik <umar.gondal6198@gmail.com>
2019-04-11 15:14:37 -0400

    added cbi functionality

--- model.py
+++ model.py
@@ -10,12 +10,59 @@ from urllib.request import urlopen
 import os.path
 from utils import API_URL_BASE, API_SERVER
 from utils import get_header_basic_auth
+import jinja2 as _jinja2
+import shutil as _shutil
 
+
+# _default_dataspine_templates_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'templates'))
 
 @click.group()
 def model():
     """The family commands to work with model"""
     pass
+
+@model.command()
+@click.option('--model-name', 'model_name', prompt="Model name", help='Name of the model')
+@click.option('--model-tag', 'model_tag', prompt="Model tag", help='Tag of the model')
+@click.option('--model-type', 'model_type', prompt="Model type", help='Type of the model')
+@click.option('--model-runtime', 'model_runtime', prompt="Model runtime", help='Runtime of the model')
+@click.option('--model-chip', 'model_chip', prompt="Model chip", help='Compute type of the model')
+@click.option('--model-path', 'model_path', prompt="Model path", help='Model path')
+def init(model_chip, model_tag, model_name, model_runtime, model_type, model_path):
+    """Init a model in the cluster"""
+
+    url = '{}/model/init'.format(API_URL_BASE)
+    #headers = get_header_basic_auth()
+
+    body = {
+        'model_tag': model_tag,
+        'model_name': model_name,
+        'model_type': model_type,
+        'model_runtime': model_runtime,
+        'model_chip' : model_chip,
+        'model_path' : model_path,
+    }
+    _model_subdir_name = 'directory'
+
+
+    model_path = os.path.expandvars(model_path)
+    model_path = os.path.expanduser(model_path)
+    model_path = os.path.normpath(model_path)
+    absolute_model_path = os.path.abspath(model_path)
+
+    if not os.path.exists(absolute_model_path):
+        os.makedirs(absolute_model_path)
+
+    #absolute_template_file_path = _os.path.normpath(_os.path.join(templates_base_path, template))
+    absolute_model_file_directory = os.path.normpath(os.path.join(absolute_model_path, _model_subdir_name))
+    if not os.path.exists(absolute_model_file_directory):
+        os.makedirs(absolute_model_file_directory)
+
+    response = requests.get(url, json=body).json()
+    for file in response['model_files']:
+        with open(file['relative_model_file_path'], 'wt') as fh:
+                fh.write(file['file_contents'])
+    print ("System files have been initialized in your current directory")
 
 
 @model.command()
@@ -24,10 +71,87 @@ def model():
 @click.option('--model-type', prompt='Model type', help='Model Type.')
 @click.option('--model-path', prompt='Model path', help='Model Path.')
 # @click.option('--model_runtime', prompt = 'Model runtime', help = 'Model runtime.', required = False, default = None)
-def build(model_name, model_tag, model_type, model_path):
+def buildfiles(model_name, model_tag, model_type, model_path):
     """Build model container"""
 
     url = API_URL_BASE + '/buildmodel'
+    #directory = _subprocess.call('pwd', shell=True)
+    request = {
+        'model_tag': model_tag,
+        'model_name': model_name,
+        'model_type': model_type,
+        'model_path': model_path,
+        # 'model_runtime': model_runtime,
+    }
+
+    #print("Building Dockerfile!\n\n")
+    response = requests.post(url, request).json()
+    build_coordinates = response['build_coordinates']
+
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  # Only for gangstars
+
+    fileContent = urlopen(response['dockerFileUrl'], context=gcontext).read().decode('utf-8')
+    basePath = os.path.join(os.path.expanduser(model_path))
+    filePath = os.path.join(basePath, response['dockerFileName'])
+    Dockerfilepath = os.path.join(basePath, 'Dockerfile')
+
+    f = open(filePath, 'w')
+    f.write(fileContent)
+    f.close()
+
+    f = open(Dockerfilepath, 'w')
+    f.write(fileContent)
+    f.close()
+
+    print ("Model files built and are ready to be pushed to Git")
+
+
+
+    # cmd = 'docker build %s -f %s %s' % (build_coordinates, filePath, basePath)
+    # process = _subprocess.call(cmd, shell=True)
+    #  if (response['command']):
+    #    print(response['command'])
+    #    process = _subprocess.call(response['command'], shell = True)
+    #  else:
+    #    print("Something went wrong!")
+    # print(response)
+
+# @model.command()
+# @click.option('--model-name', prompt='Model name', help='Model Name.')
+# @click.option('--model-tag', prompt='Model tag', help='Model Tag.')
+# @click.option('--model-type', prompt='Model type', help='Model Type.')
+# @click.option('--model-path', prompt='Model path', help='Model Path.')
+# # @click.option('--model_runtime', prompt = 'Model runtime', help = 'Model runtime.', required = False, default = None)
+# def build(model_name, model_tag, model_type, model_path):
+#     """Build model container"""
+
+#     url = API_URL_BASE + '/buildfile'
+#     directory = _subprocess.call('pwd', shell=True)
+#     request = {
+#         'model_tag': model_tag,
+#         'model_name': model_name,
+#         'model_type': model_type,
+#         'model_path': model_path,
+#         # 'model_runtime': model_runtime,
+#     }
+
+#     print("Building Dockerfile!\n\n")
+#     response = requests.post(url, request).json()
+#     print(response['status'])
+    
+
+
+
+@model.command()
+@click.option('--model-name', prompt='Model name', help='Model Name.')
+@click.option('--model-tag', prompt='Model tag', help='Model Tag.')
+@click.option('--model-type', prompt='Model type', help='Model Type.')
+@click.option('--model-path', prompt='Model path', help='Model Path.')
+# @click.option('--model_runtime', prompt = 'Model runtime', help = 'Model runtime.', required = False, default = None)
+def train(model_name, model_tag, model_type, model_path):
+    """Build train container"""
+
+    url = API_URL_BASE + '/build/trainserver'
     directory = _subprocess.call('pwd', shell=True)
     request = {
         'model_tag': model_tag,
@@ -61,6 +185,8 @@ def build(model_name, model_tag, model_type, model_path):
     # print(response)
 
 
+
+
 @model.command()
 @click.option('--model-name', prompt='Model name', help='Model Name.')
 @click.option('--model-tag', prompt='Model tag', help='Model Tag.')
@@ -78,6 +204,39 @@ def push(model_name, model_tag):
     print(cmd)
     print("")
     process = _subprocess.call(cmd, shell=True)
+
+
+@model.command()
+@click.option('--git-url', prompt='Git URL', help='Git URL')
+@click.option('--model-name', prompt='Model name', help='Model name')
+@click.option('--model-tag', prompt='Model tag', help='Model tag')
+def build(git_url, model_name, model_tag):
+    """Build model and push to registry"""
+    url = API_URL_BASE + '/model/cbi/build'
+    request = {
+        'git_url': git_url,
+        'model_name': model_name,
+        'model_tag': model_tag,
+        # 'model_runtime': model_runtime,
+    }
+    response = requests.post(url, request).json()
+
+    #response1 = response.json()
+    print(response['message'])
+
+
+@model.command()
+@click.option('--model-name', prompt='Model name', help='Model name')
+@click.option('--model-tag', prompt='Model tag', help='Model tag')
+def buildlogs(model_name, model_tag):
+    url = API_URL_BASE + '/model/logs'
+    request = {
+         'model_name': model_name,
+         'model_tag': model_tag,
+    }
+    response = requests.post(url, request).json()
+    print(response["logs"])
+
 
 
 @model.command()
@@ -141,6 +300,26 @@ def deploy(cluster_name, cluster_namespace, model_tag, model_name):
     response1 = response.json()
     print(response1['message'])
 
+
+
+
+@model.command()
+@click.option('--model-tag', 'model_tag', prompt="Model tag", help='Tag of the model')
+@click.option('--model-name', 'model_name', prompt="Model name", help='Name of the model')
+def traincluster(model_tag, model_name):
+    """Deploy a training job in the cluster"""
+
+    url = '{}/build/traincluster'.format(API_URL_BASE)
+    headers = get_header_basic_auth()
+
+    body = {
+        'model_tag': model_tag,
+        'model_name': model_name,
+    }
+
+    response = requests.post(url, headers=headers, json=body)
+    response1 = response.json()
+    print(response1['message'])
 
 
 @model.command('endpoint')
