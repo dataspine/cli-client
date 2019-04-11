5a6f370c7b11cc3b465bf0ffc333824d2fd88202
umar-sik <umar.gondal6198@gmail.com>
2019-04-11 16:37:30 -0400

    api-server IP added

--- utils/constants.py
+++ utils/constants.py
@@ -5,9 +5,9 @@ HOME = str(Path.home())
 
 BASE_URL = 'http://localhost'
 BASE_URL_BACKEND = 'http://35.243.188.142'
-BASE_API_SERVER = 'http://35.231.59.144'
+BASE_API_SERVER = 'http://35.227.88.236'
 PORT = 5000
-API_URL_BASE = "{}:{}".format(BASE_URL, PORT)
+API_URL_BASE = "{}:{}".format(BASE_API_SERVER, PORT)
 API_BACKEND = "{}:{}".format(BASE_URL_BACKEND, PORT)
 API_SERVER = "{}:{}".format(BASE_API_SERVER, PORT)
 
