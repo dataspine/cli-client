53ccdd3109c731f5836863de1e59bc207e54b9b7
umar-sik <umar.gondal6198@gmail.com>
2019-04-12 18:52:06 -0400

    updated server ip

--- utils/constants.py
+++ utils/constants.py
@@ -5,7 +5,7 @@ HOME = str(Path.home())
 
 BASE_URL = 'http://localhost'
 BASE_URL_BACKEND = 'http://35.243.188.142'
-BASE_API_SERVER = 'http://34.73.215.141'
+BASE_API_SERVER = 'http://35.243.225.87'
 PORT = 5000
 API_URL_BASE = "{}:{}".format(BASE_API_SERVER, PORT)
 API_BACKEND = "{}:{}".format(BASE_URL_BACKEND, PORT)
