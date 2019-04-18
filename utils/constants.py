525d3f5d847d0752f39427af11129c1f61770468
umar-sik <umar.gondal6198@gmail.com>
2019-04-18 18:52:01 -0400

    urls updated

--- utils/constants.py
+++ utils/constants.py
@@ -4,9 +4,9 @@ from pathlib import Path
 HOME = str(Path.home())
 
 BASE_URL = 'http://localhost'
-BASE_URL_BACKEND = 'http://35.243.188.142'
-BASE_API_SERVER = 'http://35.243.225.87'
-PORT = 5000
+BASE_URL_BACKEND = 'http://35.237.203.61'
+BASE_API_SERVER = 'http://34.74.177.64'
+PORT = 80
 API_URL_BASE = "{}:{}".format(BASE_API_SERVER, PORT)
 API_BACKEND = "{}:{}".format(BASE_URL_BACKEND, PORT)
 API_SERVER = "{}:{}".format(BASE_API_SERVER, PORT)
