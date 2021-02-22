deb68fdc92e3b4af2843e9b1a9194ad33b1ae468
xspeed101 <xqoyan@gmail.com>
2021-02-22 11:04:07 -0500

    Removed account UUID prompts from cli

--- cli.py
+++ cli.py
@@ -46,7 +46,7 @@ main.add_command(predict)
 @main.command()
 @click.option('--username', prompt='Username', help='User Name.')
 @click.option('--password', prompt='Password', help='User Password.', hide_input=True)
-@click.option('--account-uuid', prompt='Account UUID', help='User Account UUID.', hide_input=False)
+@click.option('--account-uuid', help='User Account UUID.', hide_input=False)
 def login(username, password, account_uuid=os.environ['POD_NAMESPACE']):
     """Authentication for Dataspine"""
     url = API_URL_BASE+"/login"
@@ -54,7 +54,8 @@ def login(username, password, account_uuid=os.environ['POD_NAMESPACE']):
     encoded_str = "Basic " + (base64.b64encode(string_name.encode('utf-8'))).decode('utf-8')
 
     headers = {
-        "authorization": encoded_str
+        "authorization": encoded_str,
+        "x-account-uuid": account_uuid
     }
     try:
         from json.decoder import JSONDecodeError
@@ -101,7 +102,7 @@ def help():
 
 
 @main.command()
-@click.option('--account-uuid', prompt='Account UUID', help='User Account UUID.', hide_input=False)
+@click.option('--account-uuid', help='User Account UUID.', hide_input=False)
 def init(account_uuid):
    """Init command on main group"""
    url = API_URL_BASE + '/init'
