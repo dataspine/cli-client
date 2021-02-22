95f70fa3da81146effcec33f3a6ab0ae1d8c70af
xspeed101 <xqoyan@gmail.com>
2021-02-22 10:22:46 -0500

    Removed account UUID prompts from cli

--- cli.py
+++ cli.py
@@ -46,7 +46,6 @@ main.add_command(predict)
 @main.command()
 @click.option('--username', prompt='Username', help='User Name.')
 @click.option('--password', prompt='Password', help='User Password.', hide_input=True)
-@click.option('--account-uuid', prompt='Account UUID', help='User Account UUID.', hide_input=False)
 def login(username, password, account_uuid=os.environ['POD_NAMESPACE']):
     """Authentication for Dataspine"""
     url = API_URL_BASE+"/login"
